7cb771e81b1a9b8fdc442bcdb5ab8959557a11db
Jan T. Sott <git@idleberg.com>
2025-10-30 00:49:54 +0100

    refactor: split into multiple modules

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
 #!/usr/bin/env node
 
-import { spawn } from 'node:child_process';
-import { resolve } from 'node:path';
-import { program } from 'commander';
-import { loadEnv } from 'vite';
+import { handleCli } from './cli.ts';
 import { logger } from './log.ts';
-import { getVersion } from './utils.ts';
+import { spawnProcess } from './utils.ts';
 
-program
-	.version(await getVersion())
-	.configureOutput({
-		writeErr: (message: string) => logger.error(message),
-	})
-	.arguments('<command> [args...]')
-	.optionsGroup('loadEnv API')
-	.requiredOption('-m, --mode <string>', 'a Vite mode to determine .env file', process.env.MODE)
-	.option('-e, --envdir <string>', 'directory to load .env from', process.cwd())
-	.option('-p, --prefix <string...>', 'filter environment variables by prefix', '')
-	.optionsGroup('Advanced Options')
-	.option('-D, --debug', 'print additional debug output')
-	.option('-R, --dry-run', 'skip executing the spawned process')
-
-	// This is required to pass on unknown options to the spawned process.
-	.allowUnknownOption(true);
-
-program.parse();
-
-const [command, ...commandArgs] = program.args;
-const options = program.opts();
+const { command, commandArgs, options } = await handleCli();
 
 if (process.env.MODE === options.mode) {
 	logger.info('Mode has been derived from environment.');
@@ -41,51 +18,4 @@ if (options.debug) {
 	});
 }
 
-spawnProcess(command as string, commandArgs);
-
-function spawnProcess(command: string, args: string[] = []) {
-	const resolvedEnvDir = resolve(options.envdir);
-	const env = loadEnv(options.mode, resolvedEnvDir, options.prefix);
-
-	if (options.debug) {
-		logger.debug('Environment', env);
-	}
-
-	if (options.dryRun) {
-		const fullCommand = args.length ? `${command} ${args.join(' ')}` : command;
-		logger.info(`Dry run, not executing "${fullCommand}"`);
-		return;
-	}
-
-	const child = spawn(command, args, { stdio: 'inherit', env });
-
-	child.on('exit', (exitCode, signal: NodeJS.Signals) => {
-		if (typeof exitCode === 'number') {
-			if (options.debug) {
-				logger.debug('Exit code', exitCode);
-			}
-
-			process.exit(exitCode);
-		}
-
-		logger.info(`Process terminated with ${signal}`);
-		process.kill(process.pid, signal);
-	});
-
-	child.on('error', (error) => logger.error(error.message));
-
-	for (const signal of [
-		'SIGINT',
-		'SIGTERM',
-		'SIGPIPE',
-		'SIGHUP',
-		'SIGBREAK',
-		'SIGWINCH',
-		'SIGUSR1',
-		'SIGUSR2',
-	] as NodeJS.Signals[]) {
-		process.on(signal, () => {
-			child.kill(signal);
-		});
-	}
-}
+spawnProcess(command as string, commandArgs, options);
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
+import { spawn } from 'node:child_process';
 import { readFile } from 'node:fs/promises';
 import { resolve } from 'node:path';
+import type { OptionValues } from 'commander';
+import { loadEnv } from 'vite';
+import { logger } from './log.ts';
 
 export async function getVersion(): Promise<string> {
 	const manifestPath = resolve(import.meta.dirname, '../package.json');
@@ -7,4 +11,51 @@ export async function getVersion(): Promise<string> {
 	const { version } = JSON.parse(fileContents);
 
 	return version ?? 'development';
+}
+
+export function spawnProcess(command: string, args: string[] = [], options: OptionValues = {}) {
+	const resolvedEnvDir = resolve(options.envdir);
+	const env = loadEnv(options.mode, resolvedEnvDir, options.prefix);
+
+	if (options.debug) {
+		logger.debug('Environment', env);
+	}
+
+	if (options.dryRun) {
+		const fullCommand = args.length ? `${command} ${args.join(' ')}` : command;
+		logger.info(`Dry run, not executing "${fullCommand}"`);
+		return;
+	}
+
+	const child = spawn(command, args, { stdio: 'inherit', env });
+
+	child.on('exit', (exitCode, signal: NodeJS.Signals) => {
+		if (typeof exitCode === 'number') {
+			if (options.debug) {
+				logger.debug('Exit code', exitCode);
+			}
+
+			process.exit(exitCode);
+		}
+
+		logger.info(`Process terminated with ${signal}`);
+		process.kill(process.pid, signal);
+	});
+
+	child.on('error', (error) => logger.error(error.message));
+
+	for (const signal of [
+		'SIGINT',
+		'SIGTERM',
+		'SIGPIPE',
+		'SIGHUP',
+		'SIGBREAK',
+		'SIGWINCH',
+		'SIGUSR1',
+		'SIGUSR2',
+	] as NodeJS.Signals[]) {
+		process.on(signal, () => {
+			child.kill(signal);
+		});
+	}
 }
